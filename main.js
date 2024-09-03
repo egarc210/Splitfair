2d42bc7797bc6912df74f2b2174b8678d3cfede1
egarc210 <egarc210@hotmail.com>
2024-09-02 20:23:01 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+/*
+
+myVariable = 123
+console.log(myVariable)
+
+let myString1 = "Hello";
+let myString2 = "World";
+
+console.log(myString1 + myString2 + "!");
+console.log(`${myString1} ${myString2}`)
+
+function displayGreeting() {
+    console.log('Hello, world!')
+}
+
+function displayGreetingName(name) { //función and parameter
+    const message = `Hello, ${name}!`
+    console.log(message)
+} 
+
+displayGreetingName('Erick');
+
+setTimeout(
+    () => {
+        console.log('Done!');
+    },
+    5000
+)
+
+let currentMoney = 1500;
+let laptopPrice = 1500;
+
+if(currentMoney >= laptopPrice){
+    console.log("Getting a new laptop");
+} else{
+    console.log("Can't afford a new laptop, yet!")
+}
+
+let iceCreamFlavors = ["Chocolate", "Strawberry", "Vanilla", "Pistachio", "Neapolitan"];
+
+console.log(iceCreamFlavors[3])
+
+iceCreamFlavors.push("Mint Chip"); //Agregar un nuevo sabor
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+*/
+
+var saludar = "Hola mundo";
+
+let saludar1 = "Hello world";
+
+//alert(saludar1)
+
+console.log(window);
+
+var alert = "Italiannis";
+//alert(saludar);
+
+
+let mascotas = ["perro", "gato", "conejo", "iguana", "serpiente",["perico", "cerdo"]]
+
+console.log[1];
+
+console.log(mascotas[5][1]);
+
+let persona = {
+    nombre: "Erick",
+    edad: 33
+}
+console.log(persona.edad)
+
+
+
+
+
+
